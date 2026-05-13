195f99f65ebb43075a6a372b992ca2d6a683aebb
Pedro Robinet <peporobinet01@gmail.com>
2026-05-13 19:53:51 -0300

    prueba de branch dev

---
+++ apps/web/src/modules/Navbar.tsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <img src="/logo.svg" alt="Kinescius Hub" />
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
