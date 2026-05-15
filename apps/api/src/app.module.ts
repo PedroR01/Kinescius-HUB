ae751c0aef98051bb56afe9a4af841f881a76f3f
Justina <justinaguerrerounlp@gmail.com>
2026-05-14 21:46:01 -0300

    ConexionBD

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ThrottlerModule } from "@nestjs/throttler";
-import { SupabaseService } from "./integrations/supabase.service";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { SupabaseModule } from "./integrations/supabase.module";
+import { ClasesModule } from "./clases/clases.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 60 }]),
-    SupabaseService
-  ]
+    ThrottlerModule.forRoot({
+      throttlers: [{ ttl: 60, limit: 60 }]
+    }),
+    SupabaseModule,
+    ClasesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService]
 })
-export class AppModule { }
+export class AppModule {}
