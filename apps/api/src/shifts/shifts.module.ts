9795e5501c502f0f7a4155a0ef67df4419431c4d
Nicolas Diaz <nicodiaz421@gmail.com>
2026-05-31 18:32:32 -0300

    Fix enviar recordatorio de turno / ver mis clases (#40)
    * Actualización de interfaz Manejo de turnos
    
    * fix enviar recordatorio de turno

--- apps/api/src/shifts/shifts.module.ts
+++ apps/api/src/shifts/shifts.module.ts
@@ -5,11 +5,13 @@ import { ShiftsController } from './shifts.controller';
 import { ShiftsService } from './shifts.service';
 import { SupabaseModule } from '../integrations/supabase/supabase.module';
 import { ConfirmarTurnoModule } from '../confirmarTurno/confirmarTurno.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     SupabaseModule,
     ConfirmarTurnoModule,
+    NotificationsModule,
   ],
   controllers: [ShiftsController],
   providers: [ShiftsService],
